9cf53df9b2ccba07cc2b88851949935f09ed52ac
Psycho <56837234+psychopattt@users.noreply.github.com>
2025-12-10 20:34:32 -0500

    Lava: Increase mobility in gases

--- Particle Simulation/Particle Simulation/Source/Shaders/Update/UpdateLava.glsl
+++ Particle Simulation/Particle Simulation/Source/Shaders/Update/UpdateLava.glsl
@@ -27,14 +27,14 @@ bool IsMovableByLava(Particle particle, float random)
     {
         case VOID: return random < 0.9;
         case WATER: return random < 0.21;
-        case SMOKE: return random < 0.65;
+        case SMOKE: return random < 0.68;
         case FIRE: return random < 0.6;
         case KEROSENE: return random < 0.24;
         case STEAM: return random < 0.7;
         case SEAWATER: return random < 0.21;
         case ACID: return random < 0.28;
-        case METHANE: return random < 0.68;
-        case AMMONIA: return random < 0.68;
+        case METHANE: return random < 0.69;
+        case AMMONIA: return random < 0.69;
         default: return false;
     }
 }
