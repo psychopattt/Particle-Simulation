eb90b3cf9b5db7f620d9f4b42bede179713b07ed
Psycho <56837234+psychopattt@users.noreply.github.com>
2026-05-16 10:21:43 -0400

    Simplify particle coloring

--- Particle Simulation/Particle Simulation/Source/Shaders/PostProcessing/Color.glsl
+++ Particle Simulation/Particle Simulation/Source/Shaders/PostProcessing/Color.glsl
@@ -14,41 +14,41 @@ layout(std430) restrict readonly buffer particlesBuffer {
 
 #include "Particles.glsl"
 
-vec3 GetParticleColor(Particle particle)
+vec3 GetParticleColor(int type, float shade)
 {
-    switch (particle.type)
+    switch (type)
     {
         case AIR: return airColor;
-        case SAND: return particle.shade * 0.125 + vec3(0.82, 0.82, 0);
-        case WATER: return vec3(0, 0, particle.shade * 0.025 + 0.8);
-        case WALL: return vec3(particle.shade * 0.01 + 0.293);
-        case WOOD: return particle.shade * 0.02 + vec3(0.275, 0.157, 0);
-        case SMOKE: return vec3(particle.shade * 0.2 + 0.22);
-        case FIRE: return vec3(0.8, (particle.shade + 0.5) * 0.75, 0);
-        case ROCK: return vec3(particle.shade * 0.025 + 0.24);
-        case PETROL: return vec3(0.73, particle.shade * 0.015 + 0.6, 0.08);
-        case STEAM: return vec3(particle.shade * 0.03 + 0.78);
-        case SEAWATER: return particle.shade * 0.015 + vec3(0, 0.1, 0.7);
-        case LAVA: return vec3(0.76, particle.shade * 0.05 + 0.33, 0);
-        case ICE: return vec3(particle.shade * 0.34 + 0.17, 0.49, 0.7);
-        case SALT: return particle.shade * 0.04 + vec3(0.98, 0.97, 0.94);
-        case SAWDUST: return particle.shade * vec3(0, 0.05, 0.1) + vec3(0.67, 0.54, 0.33);
-        case ACID: return vec3(particle.shade * 0.3 + 0.25, 0.9, 0);
-        case VINE: return vec3(0, particle.shade * 0.15 + 0.225, 0.1);
-        case RUST: return particle.shade * vec3(0.46, 0.16, 0) + vec3(0.34, 0.12, 0);
-        case IRON: return particle.shade * 0.01 + vec3(0.31, 0.34, 0.39);
-        case METHANE: return particle.shade * 0.02 + vec3(0.65, 0.77, 0.8);
-        case AMMONIA: return particle.shade * 0.03 + vec3(0.73, 0.71, 0.66);
-        case CHLORINE: return particle.shade * 0.014 + vec3(0.54, 0.56, 0.1);
-        case WAX: return vec3(vec2(particle.shade * -0.015 + 0.7), 0.5);
-        case MERCURY: return particle.shade * 0.015 + vec3(0.4, 0.41, 0.42);
-        case CEMENT: return particle.shade * 0.028 + vec3(0.65, 0.65, 0.63);
+        case SAND: return shade * 0.125 + vec3(0.82, 0.82, 0);
+        case WATER: return vec3(0, 0, shade * 0.025 + 0.8);
+        case WALL: return vec3(shade * 0.01 + 0.293);
+        case WOOD: return shade * 0.02 + vec3(0.275, 0.157, 0);
+        case SMOKE: return vec3(shade * 0.2 + 0.22);
+        case FIRE: return vec3(0.8, (shade + 0.5) * 0.75, 0);
+        case ROCK: return vec3(shade * 0.025 + 0.24);
+        case PETROL: return vec3(0.73, shade * 0.015 + 0.6, 0.08);
+        case STEAM: return vec3(shade * 0.03 + 0.78);
+        case SEAWATER: return shade * 0.015 + vec3(0, 0.1, 0.7);
+        case LAVA: return vec3(0.76, shade * 0.05 + 0.33, 0);
+        case ICE: return vec3(shade * 0.34 + 0.17, 0.49, 0.7);
+        case SALT: return shade * 0.04 + vec3(0.98, 0.97, 0.94);
+        case SAWDUST: return shade * vec3(0, 0.05, 0.1) + vec3(0.67, 0.54, 0.33);
+        case ACID: return vec3(shade * 0.3 + 0.25, 0.9, 0);
+        case VINE: return vec3(0, shade * 0.15 + 0.225, 0.1);
+        case RUST: return shade * vec3(0.46, 0.16, 0) + vec3(0.34, 0.12, 0);
+        case IRON: return shade * 0.01 + vec3(0.31, 0.34, 0.39);
+        case METHANE: return shade * 0.02 + vec3(0.65, 0.77, 0.8);
+        case AMMONIA: return shade * 0.03 + vec3(0.73, 0.71, 0.66);
+        case CHLORINE: return shade * 0.014 + vec3(0.54, 0.56, 0.1);
+        case WAX: return vec3(vec2(shade * -0.015 + 0.7), 0.5);
+        case MERCURY: return shade * 0.015 + vec3(0.4, 0.41, 0.42);
+        case CEMENT: return shade * 0.028 + vec3(0.65, 0.65, 0.63);
         case CLONER: return vec3(0.5, 0.4, 0.06);
         case RANDOM: return vec3(0.2, 0, 0.25);
-        case FOAM: return particle.shade * 0.0009 + vec3(0.6, 0.76, 0.63);
-        case BRICK: return particle.shade * 0.06 + vec3(0.49, 0.18, 0.18);
-        case GRASS: return vec3(particle.shade * 0.012, 0.3 + max(0, (particle.shade - 16) * 8), 0);
-        case KELP: return particle.shade * 0.04 + vec3(0.39, 0.24, 0.02);
+        case FOAM: return shade * 0.0009 + vec3(0.6, 0.76, 0.63);
+        case BRICK: return shade * 0.06 + vec3(0.49, 0.18, 0.18);
+        case GRASS: return vec3(shade * 0.012, 0.3 + max(0, (shade - 16) * 8), 0);
+        case KELP: return shade * 0.04 + vec3(0.39, 0.24, 0.02);
         default: return vec3(1, 0, 1);
     }
 }
@@ -61,6 +61,8 @@ void main()
         return;
 
     uint id = position.y * size.x + position.x;
-    vec3 color = GetParticleColor(Particles[id]);
+    Particle particle = Particles[id];
+
+    vec3 color = GetParticleColor(particle.type, particle.shade);
     imageStore(texture, position, vec4(color, 1));
 }
